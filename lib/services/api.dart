636543735b957198512ec1bcabb93ccf2386ae42
Eduardo Vital Alencar Cunha <vitaldu@gmail.com>
2019-09-15 18:45:14 -0300

    add getOrderbook method to api service

--- lib/services/api.dart
+++ lib/services/api.dart
@@ -1,3 +1,4 @@
+import 'package:bit_brazil/models/orderbook.dart';
 import 'package:bit_brazil/models/ticker.dart';
 import 'package:http/http.dart' as http;
 
@@ -6,4 +7,9 @@ String baseUrl = 'https://api.bitpreco.com/btc-brl';
 Future<Ticker> getTicker() async {
   final response = await http.get('$baseUrl/ticker');
   return tickerFromJson(response.body);
+}
+
+Future<Orderbook> getOrderbook() async {
+  final response = await http.get('$baseUrl/orderbook');
+  return orderbookFromJson(response.body);
 }
