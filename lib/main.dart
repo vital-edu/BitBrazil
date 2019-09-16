c5375b207672429a3b83d88ba79a76b7e62db116
Eduardo Vital Alencar Cunha <vitaldu@gmail.com>
2019-09-15 21:27:36 -0300

    create OrderWidget

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:bit_brazil/services/api.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter/material.dart';
 
+import 'models/order.dart';
+
 main() => runApp(HomeWidget());
 
 class HomeWidget extends StatelessWidget {
@@ -11,31 +13,67 @@ class HomeWidget extends StatelessWidget {
         appBar: AppBar(
           title: const Text('Bit Brazil'),
         ),
-        body: FutureBuilder(
-          builder: (context, snap) {
-            if (snap.connectionState == ConnectionState.done) {
-              return ListView.builder(
-                padding: EdgeInsets.all(8),
-                itemCount: snap.data.bids.length,
-                itemBuilder: (BuildContext context, int index) {
-                  return Container(
-                    child: Text(snap.data.bids[index].price.toString()),
-                    height: 50,
-                  );
-                },
-              );
-            } else {
-              return Center(
-                child: const Text(
-                  'Loading data...',
-                  style: TextStyle(fontSize: 32),
-                ),
-              );
-            }
-          },
-          future: getOrderbook()
-        )
+        body: new OrderbookWidget()
       )
+    );
+  }
+}
+
+class OrderbookWidget extends StatelessWidget {
+  const OrderbookWidget({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      builder: (context, snap) {
+        if (snap.connectionState == ConnectionState.done) {
+          List<Order> orders = snap.data.asks + snap.data.bids;
+          return ListView.builder(
+            padding: EdgeInsets.all(8),
+            itemCount: orders.length,
+            itemBuilder: (BuildContext context, int index) {
+                return OrderWidget(
+                  order: orders[index],
+                  color: index < snap.data.asks.length ? Colors.red : Colors.blue,
+                );
+            },
+          );
+        } else {
+          return Center(
+            child: const Text(
+              'Loading data...',
+              style: TextStyle(fontSize: 32),
+            ),
+          );
+        }
+      },
+      future: getOrderbook()
+    );
+  }
+}
+
+class OrderWidget extends StatelessWidget {
+  const OrderWidget({
+    Key key,
+    @required this.order,
+    @required this.color,
+  }) : super(key: key);
+
+  final Order order;
+  final Color color;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      // color: Colors.red,
+      child: ListTile(
+        title: Text(
+          order.price.toString(),
+          style: TextStyle(color: color),
+        ),
+      ),
     );
   }
 }
