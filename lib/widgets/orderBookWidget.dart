c5baae3679bad2a6f4a58d1c8b4c60b6d69e19fc
Eduardo Vital Alencar Cunha <vitaldu@gmail.com>
2019-09-16 00:19:11 -0300

    add order spread

--- lib/widgets/orderBookWidget.dart
+++ lib/widgets/orderBookWidget.dart
@@ -15,33 +15,60 @@ class OrderbookWidget extends StatelessWidget {
     return FutureBuilder(
       builder: (context, snap) {
         if (snap.connectionState == ConnectionState.done) {
-          List<Order> orders = snap.data.asks + snap.data.bids;
+          List<Order> asks = snap.data.asks;
+          List<Order> bids = snap.data.bids;
+          double ask = asks.last.price.toDouble();
+          double bid = bids.last.price.toDouble();
+          double spread = ask - bid;
+
           return Column(
             children: <Widget>[
               OrderbookHeaderWidget(),
               Expanded(
                 child: ListView.builder(
                   padding: EdgeInsets.all(8),
-                  itemCount: orders.length,
+                  itemCount: asks.length,
                   itemBuilder: (BuildContext context, int index) {
                     OrderWidgetStyle orderStyle;
-                    if (index < snap.data.asks.length - 1) {
+                    if (index < asks.length - 1) {
                       orderStyle = OrderWidgetStyle.askOrder;
-                    } else if (index == snap.data.asks.length - 1) {
+                    } else if (index == asks.length - 1) {
                       orderStyle = OrderWidgetStyle.highlightedAskOrder;
-                    } else if (index == snap.data.asks.length) {
+                    }
+
+                    return OrderWidget(
+                      order: asks[index],
+                      style: orderStyle,
+                    );
+                  },
+                )
+              ),
+              ListTile(
+                  title: Text(
+                    '${spread.toStringAsFixed(2)} BRL spread',
+                    style: TextStyle(color: Colors.white),
+                    textAlign: TextAlign.center,
+                  ),
+              ),
+              Expanded(
+                child: ListView.builder(
+                  padding: EdgeInsets.all(8),
+                  itemCount: bids.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    OrderWidgetStyle orderStyle;
+                    if (index == 0) {
                       orderStyle = OrderWidgetStyle.highlighteBidOrder;
                     } else {
                       orderStyle = OrderWidgetStyle.bidOrder;
                     }
 
                     return OrderWidget(
-                      order: orders[index],
+                      order: bids[index],
                       style: orderStyle,
                     );
                   },
                 )
-              )
+              ),
             ],
           );
         } else {
